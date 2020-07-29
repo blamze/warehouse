d1ff6aef075a9bce32735acc77cc9f9da98d781d
Edmundas <edmas13@gmail.com>
2020-07-30 00:51:29 +0300

    Updated Title atom

--- src/components/atoms/Title/Title.tsx
+++ src/components/atoms/Title/Title.tsx
@@ -1,9 +1,10 @@
 import React, { FunctionComponent } from 'react';
 import Typography from '@material-ui/core/Typography';
 
-export const Title: FunctionComponent = ({ children }) => (
+const Title: FunctionComponent = ({ children }) => (
   <Typography component="h2" variant="h6" color="primary" gutterBottom>
     {children}
   </Typography>
 );
 
+export default Title
